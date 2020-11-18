f2b0ac811b8da7cf6bb8d76b47f8f53065301d0b
bittencourt-lucas <lucascafieiro@hotmail.com>
2020-11-18 20:01:08 -0300

    IOException handling em UserDAO

--- src/infra/UserDAO.java
+++ src/infra/UserDAO.java
@@ -6,21 +6,29 @@ import java.util.ArrayList;
 public class UserDAO {
 
     public static void salvaUsers(ArrayList<User> users, String filePath) throws Exception {
-        FileOutputStream fos = new FileOutputStream(filePath);
-        ObjectOutputStream oos = new ObjectOutputStream(fos);
-        oos.writeObject(users);
-        oos.flush();
-        oos.close();
-        fos.close();
+        try {
+            FileOutputStream fos = new FileOutputStream(filePath);
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            oos.writeObject(users);
+            oos.flush();
+            oos.close();
+            fos.close();
+        } catch (IOException exception) {
+            throw new IOException("Erro ao salvar usuário!", exception);
+        }
     }
 
 
     public static ArrayList<User> lerUsers(String path) throws Exception {
-        FileInputStream fis = new FileInputStream(path);
-        ObjectInputStream ois = new ObjectInputStream(fis);
-        ArrayList<User> users = (ArrayList<User>) ois.readObject();
-        ois.close();
-        fis.close();
-        return users;
+        try {
+            FileInputStream fis = new FileInputStream(path);
+            ObjectInputStream ois = new ObjectInputStream(fis);
+            ArrayList<User> users = (ArrayList<User>) ois.readObject();
+            ois.close();
+            fis.close();
+            return users;
+        } catch (IOException exception) {
+            throw new IOException("Erro ao ler usuários!", exception);
+        }
     }
 }
